63e06ba5777b8b20f7048b7041e3a78659f21a83
charlie <linkin8288@gmail.com>
2023-07-18 10:10:30 +0200

    add all category

--- src/app/features/category/services/category.service.ts
+++ src/app/features/category/services/category.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AddCategoryRequest } from '../models/add-category-request.model';
+import { Category } from '../models/category.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +14,11 @@ export class CategoryService {
   constructor(private http: HttpClient) { }
 
   addCategory(model: AddCategoryRequest): Observable<void> {
-    return this.http.post<void>('https://localhost:7075/api/categories', model);
+    return this.http.post<void>(`${environment.apiBaseUrl}/api/categories`, model);
+  }
+
+  getAllCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/categories`);
+
   }
 }
